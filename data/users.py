d4628cdd8119bce9f739992aa31f0e1d093f7e89
William Lin <william@osiris.cyber.nyu.edu>
2024-10-10 17:57:49 -0400

    fixed errors

--- data/users.py
+++ data/users.py
@@ -45,25 +45,24 @@ def read():
     users = get_users()
     return users
 
+
 def update_users(uname, new_level):
     """
     Update the user's level in the users dictionary.
-    
+
     Args:
         user_name (str): The name of the user to update.
         new_level (int): The new level to assign to the user.
-        
+    
     Returns:
         dict or None: The updated user data if the user exists, None otherwise.
     """
     users = get_users()
-    
     if uname in users:
         users[uname][LEVEL] = new_level
         return users[uname]
     else:
         return None
-
 
 
 def main():
