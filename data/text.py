51c3d99ee077079e6d0093956ec8dd18b1d24262
W1LL9999 <william@osiris.cyber.nyu.edu>
2024-10-24 14:58:07 -0400

    Added substance to text.py

--- data/text.py
+++ data/text.py
@@ -1,2 +1,39 @@
+KEY = 'key'
+TITLE = 'title'
+TEXT = 'text'
+EMAIL = 'email'
+
+TEST_KEY = 'HomePage'
+SUBM_KEY = 'SubmissionsPage'
+DEL_KEY = 'DeletePage'
+
+text_dict = {
+    TEST_KEY: {
+        TITLE: 'Home Page',
+        TEXT: 'This is a journal about building API servers.',
+    },
+    SUBM_KEY: {
+        TITLE: 'Submissions Page',
+        TEXT: 'All submissions must be original work in Word format.',
+    },
+    DEL_KEY: {
+        TITLE: 'Delete Page',
+        TEXT: 'This is a text to delete.',
+    },
+}
+
+
+def create():
+    pass
+
+
+def delete():
+    pass
+
+
+def update():
+    pass
+
+
 if __name__ == '__main__':
     pass
