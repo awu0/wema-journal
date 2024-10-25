6ada58f67285a198e9ba5285b8933fb09c5a59cd
Eric Dong <108444931+EricDong2303@users.noreply.github.com>
2024-10-24 20:34:51 -0400

    Update roles.py with get_masthead_role function

--- data/roles.py
+++ data/roles.py
@@ -16,6 +16,15 @@ roles = {
 def get_roles() -> dict:
     return roles
 
+def get_masthead_roles() -> dict:
+    mh_roles = get_roles()
+    del_mh_roles = []
+    for role in mh_roles:
+        if role not in MH_ROLES:
+            del_mh_roles.append(role)
+    for del_role in del_mh_roles:
+        del mh_roles[del_role]
+    return mh_roles
 
 def get_role_codes() -> list:
     return list(roles.keys())
