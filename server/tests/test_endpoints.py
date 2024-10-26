ed9b8825d913116f474c87b03ad3a0696dc74122
Aaron Wu <82164146+awu0@users.noreply.github.com>
2024-10-25 21:07:10 -0400

    removed print statements in endpoint test

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -24,9 +24,7 @@ def test_hello():
 
 def test_title():
     resp = TEST_CLIENT.get(ep.TITLE_EP)
-    print(f'{ep.TITLE_EP=}')
     resp_json = resp.get_json()
-    print(f'{resp_json=}')
     assert ep.TITLE_RESP in resp_json
     assert isinstance(resp_json[ep.TITLE_RESP], str)
     assert len(resp_json[ep.TITLE_RESP]) > 0
