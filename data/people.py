26a53e38467efe2e49854c4a35d8b6ff4d61aa96
W1LL9999 <william@osiris.cyber.nyu.edu>
2024-11-07 19:31:01 -0500

    Initialize people.py

---
+++ data/people.py
@@ -0,0 +1,12 @@
+"""
+This module interfaces to our user data.
+"""
+MIN_USER_NAME_LEN = 2
+# fields
+NAME = 'name'
+ROLES = 'roles'
+AFFILIATION = 'affiliation'
+EMAIL = 'email'
+
+TEST_EMAIL = 'ejc369@nyu.edu'
+DEL_EMAIL = 'delete@nyu.edu'
