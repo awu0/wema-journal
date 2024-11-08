471cc319f267fa2fef85daeff3025209ea3d6864
W1LL9999 <william@osiris.cyber.nyu.edu>
2024-11-07 19:35:34 -0500

    added people_dict and fixed flake8

--- data/people.py
+++ data/people.py
@@ -1,8 +1,12 @@
 """
-This module interfaces to our user data.
+This module interfaces with our user data.
 """
+
+import data.roles as rls
+
 MIN_USER_NAME_LEN = 2
-# fields
+
+# Fields
 NAME = 'name'
 ROLES = 'roles'
 AFFILIATION = 'affiliation'
@@ -10,3 +14,18 @@ EMAIL = 'email'
 
 TEST_EMAIL = 'ejc369@nyu.edu'
 DEL_EMAIL = 'delete@nyu.edu'
+
+people_dict = {
+    TEST_EMAIL: {
+        NAME: 'Eugene Callahan',
+        ROLES: [rls.ED_CODE],
+        AFFILIATION: 'NYU',
+        EMAIL: TEST_EMAIL,
+    },
+    DEL_EMAIL: {
+        NAME: 'Jeffrey Person',
+        ROLES: [rls.CE_CODE],
+        AFFILIATION: 'NYU',
+        EMAIL: DEL_EMAIL,
+    },
+}
