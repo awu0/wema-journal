026e4284a9378f65920e8629b08332afb1af4155
mm11762 <matthewma929@gmail.com>
2024-12-05 17:05:48 -0500

    fixed naming

--- data/manuscripts/tests/test_fields.py
+++ data/manuscripts/tests/test_fields.py
@@ -1,11 +1,11 @@
 import data.manuscripts.fields as mflds
 
 
-def test_get_flds():
-    assert isinstance(mflds.get_flds(), dict)
+def test_get_fields():
+    assert isinstance(mflds.get_fields(), dict)
 
 
-def test_get_fld_names():
-    fld_names = mflds.get_fld_names()
+def test_get_field_names():
+    fld_names = mflds.get_field_names()
     assert isinstance(fld_names, list)
     assert all(isinstance(name, str) for name in fld_names) 
