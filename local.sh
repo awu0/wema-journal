1ae4c1894c6183f08674b1fd0f2a8b501d7471b8
W1LL9999 <william@osiris.cyber.nyu.edu>
2024-12-12 22:45:19 -0500

    added additional frontend files for react

--- local.sh
+++ local.sh
@@ -4,6 +4,17 @@ export FLASK_ENV=development
 export PROJ_DIR=$PWD
 export DEBUG=1
 
-# run our server locally:
-PYTHONPATH=$(pwd):$PYTHONPATH
-FLASK_APP=server.endpoints flask run --debug --host=127.0.0.1 --port=8000
+# Run the Flask server locally in the background
+PYTHONPATH=$(pwd):$PYTHONPATH FLASK_APP=server.endpoints flask run --debug --host=127.0.0.1 --port=8000 &
+
+# Save the PID of the Flask process
+FLASK_PID=$!
+
+# Navigate to the React frontend directory
+#cd frontend
+
+# Start React app
+#npm run start
+
+# Kill the Flask server when React is stopped
+#kill $FLASK_PID
