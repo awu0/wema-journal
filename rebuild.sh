56ea7f371bfa4831e53411b9436f21e77169f94a
W1LL9999 <william@osiris.cyber.nyu.edu>
2024-12-19 23:21:27 -0500

    testing

--- rebuild.sh
+++ rebuild.sh
@@ -5,21 +5,24 @@
 touch rebuild
 echo "Rebuilding $PA_DOMAIN"
 
-echo "Pulling code from master"
-git pull origin main
+echo "Setting up mock PythonAnywhere environment in GitHub Actions"
+export PA_MOCK_HOME="/home/$PA_USER/.virtualenvs/$VENV"
+mkdir -p $PA_MOCK_HOME
+python3 -m venv $PA_MOCK_HOME
 
-echo "Activate the virtual env $VENV for user $PA_USER"
-source /home/$PA_USER/.virtualenvs/$VENV/bin/activate
+echo "Activating the virtual environment for user $PA_USER"
+source $PA_MOCK_HOME/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
 
 echo "Install packages"
 pip install --upgrade -r requirements.txt
 
 echo "Installing PythonAnywhere helper package"
-pip install --user pythonanywhere
+pip install pythonanywhere
 
 echo "Going to reboot the webserver using $API_TOKEN"
 export PYTHONANYWHERE_TOKEN=$API_TOKEN
-pa_reload_webapp.py $PA_DOMAIN
+if command -v pa_reload_webapp.py &>/dev/null; then
+  pa_reload_webapp.py $PA_DOMAIN
 
 touch reboot
 echo "Finished rebuild."
