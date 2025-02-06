b2742c2c950a2fcbe6a1a67ef9ec97aa77a4e027
Eric <ed2303@nyu.edu>
2025-02-06 11:38:54 -0500

    security

--- security/security.py
+++ security/security.py
@@ -1,3 +1,52 @@
+from functools import wraps
+
+# import data.db_connect as dbc
+
+"""
+Our record format to meet our requirements (see security.md) will be:
+
+{
+    feature_name1: {
+        create: {
+            user_list: [],
+            checks: {
+                login: True,
+                ip_address: False,
+                dual_factor: False,
+                # etc.
+            },
+        },
+        read: {
+            user_list: [],
+            checks: {
+                login: True,
+                ip_address: False,
+                dual_factor: False,
+                # etc.
+            },
+        },
+        update: {
+            user_list: [],
+            checks: {
+                login: True,
+                ip_address: False,
+                dual_factor: False,
+                # etc.
+            },
+        },
+        delete: {
+            user_list: [],
+            checks: {
+                login: True,
+                ip_address: False,
+                dual_factor: False,
+                # etc.
+            },
+        },
+    },
+    feature_name2: # etc.
+}
+"""
 
 COLLECT_NAME = 'security'
 CREATE = 'create'
