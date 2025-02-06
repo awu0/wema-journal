cb01e8c5c43e9783733af8b5c6578a89ca55a83a
Eric <ed2303@nyu.edu>
2025-02-06 11:42:11 -0500

    more security from demo repo

--- security/security.py
+++ security/security.py
@@ -73,8 +73,30 @@ temp_recs = {
     },
 }
 
+
 def read() -> dict:
     global security_recs
     # dbc.read()
     security_recs = temp_recs
     return security_recs
+
+
+def needs_recs(fn):
+    """
+    Should be used to decorate any function that directly accesses sec recs.
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        global security_recs
+        if not security_recs:
+            security_recs = read()
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@needs_recs
+def read_feature(feature_name: str) -> dict:
+    if feature_name in security_recs:
+        return security_recs[feature_name]
+    else:
+        return None
