aeb3067de6fbbc5e34d5b8fffdc3f3c3559e550e
Eric <ed2303@nyu.edu>
2025-04-08 23:57:58 -0400

    started security based on prof lecture

--- security/security.py
+++ security/security.py
@@ -57,28 +57,60 @@ DELETE = 'delete'
 USER_LIST = 'user_list'
 CHECKS = 'checks'
 LOGIN = 'login'
+LOGIN_KEY = 'login_key'
 
 # Features:
 PEOPLE = 'people'
+BAD_FEATURE = 'baaaad feature'
+
+PEOPLE_MISSING_ACTION = READ
+GOOD_USER_ID = 'ed2303@nyu.edu'
 
 security_recs = None
 # These will come from the DB soon:
-temp_recs = {
+TEST_RECS = {
     PEOPLE: {
         CREATE: {
-            USER_LIST: ['ejc369@nyu.edu'],
+            USER_LIST: [GOOD_USER_ID],
             CHECKS: {
                 LOGIN: True,
             },
         },
     },
+    BAD_FEATURE: {
+        CREATE: {
+            USER_LIST: [GOOD_USER_ID],
+            CHECKS: {
+                'Bad check': True,
+             },
+         },
+     },
 }
+
+
+def is_valid_key(user_id: str, login_key: str):
+    """
+    This is just a mock of the real is_valid_key() we'll write later.
+    """
+    return True
+
+
+def check_login(user_id: str, **kwargs):
+    if LOGIN_KEY not in kwargs:
+        return False
+    return is_valid_key(user_id, kwargs[LOGIN_KEY])
+
+
+CHECK_FUNCS = {
+    LOGIN: check_login,
+    # IP_ADDRESS: check_ip,
+ }
 
 
 def read() -> dict:
     global security_recs
     # dbc.read()
-    security_recs = temp_recs
+    security_recs = TEST_RECS
     return security_recs
 
 
@@ -195,3 +227,23 @@ def remove_user_permission(feature_name: str, operation: str, user_email: str) -
     if user_email in security_recs[feature_name][operation][USER_LIST]:
         security_recs[feature_name][operation][USER_LIST].remove(user_email)
     return  # write to database (Needs to be Implemented)
+
+
+@needs_recs
+def is_permitted(feature_name: str, action: str, user_id: str, **kwargs) -> bool:
+    prot = read_feature(feature_name)
+    if prot is None:
+        return True
+    if action not in prot:
+        return True
+    if USER_LIST in prot[action]:
+        if user_id not in prot[action][USER_LIST]:
+            return False
+    if CHECKS not in prot[action]:
+        return True
+    for check in prot[action][CHECKS]:
+        if check not in CHECK_FUNCS:
+            raise ValueError(f'Bad check passed to is_permitted: {check}')
+        if not CHECK_FUNCS[check](user_id, **kwargs):
+            return False
+    return True
